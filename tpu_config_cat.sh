44dee6f87b793f57dc99bd253f891102bf29843f
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-06 17:45:29 -0800

    Update

--- tpu_config_cat.sh
+++ tpu_config_cat.sh
@@ -19,7 +19,8 @@ touch /home/noam/.ssh/known_hosts
 ssh-keyscan github.com >> /home/noam/.ssh/known_hosts
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source ~/.bashrc
 git clone git@github.com:catglossop/bigvision-palivla.git --recursive
 cd ~/bigvision-palivla
-uv init --python=python3.10
+uv venv --python=python3.11
 uv sync --extra tpu
