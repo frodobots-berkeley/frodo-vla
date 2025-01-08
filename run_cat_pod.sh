3b645e0ed25f735f08de97dc0086b71e5646cf3f
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-08 13:35:49 -0800

    Update

--- run_cat_pod.sh
+++ run_cat_pod.sh
@@ -48,4 +48,4 @@ if $UPDATE; then
     gcloud alpha compute tpus tpu-vm ssh $TPU_VM_NAME --zone=us-central2-b --command="bash update.sh" --worker=all
 fi
 
-gcloud alpha compute tpus tpu-vm ssh $TPU_VM_NAME --zone=us-central2-b --worker=all --command='tmux kill-session -t tpc_$TPU_VM_NAME; tmux new -s tpc_$TPU_VM_NAME -d "bash -li /tmp/launch.sh $API_KEY"'
+gcloud alpha compute tpus tpu-vm ssh $TPU_VM_NAME --zone=us-central2-b --worker=all --command='tmux kill-session -t l; tmux new -s l -d "bash -li /tmp/launch.sh $API_KEY"'
