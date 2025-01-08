0c00927ecc9ad2b7fa3901a62a207237e5abec0c
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-07 18:44:57 -0800

    Update

--- configs/nav_config.py
+++ configs/nav_config.py
@@ -2,6 +2,7 @@ from octo.data.utils.data_utils import NormalizationType
 from ml_collections.config_dict import placeholder, ConfigDict, FieldReference
 
 from palivla.components.model import get_default_config
+from octo.data.oxe.oxe_standarization_transforms import gnm_dataset_transform
 
 placeholder(int)._value
 
@@ -80,7 +81,7 @@ def get_config():
                         "language_key" : "language_instruction",
                         "force_recompute_dataset_statistics": False,
                         "action_proprio_normalization_type": NormalizationType.NORMAL,
-                        "standardize_fn" : "gnm_dataset_transform"
+                        "standardize_fn" : gnm_dataset_transform,   
                     },
                 },
                 "sample_weights": [1.0],
