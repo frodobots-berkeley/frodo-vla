b793195d538b1089900f5bb02982ebfba754e6e9
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-08 14:50:50 -0800

    Update

--- launch.sh
+++ launch.sh
@@ -2,6 +2,5 @@ API_KEY=$1
 cd ~/bigvision-palivla
 source ~/.local/bin/env
 source .venv/bin/activate
-huggingface-cli login
 uv run wandb login $API_KEY
 python scripts/train.py --config configs/nav_config.py --tpu
