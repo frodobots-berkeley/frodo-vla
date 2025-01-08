e3d368349129ecb7f07516a16b6de89d1b65cdcd
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-07 20:51:49 -0800

    Update

--- src/palivla/components/sequence_builder.py
+++ src/palivla/components/sequence_builder.py
@@ -173,6 +173,7 @@ class SequenceBuilder:
             )
             for i in range(len(tokens))
         ]
+        print(actions)
 
         # Get the shape of a valid action
         action_horizon = 0
@@ -185,6 +186,7 @@ class SequenceBuilder:
         actions_mask = np.array([action is not None for action in actions])
         print(len(actions))
         print(action_horizon, action_dim)
+        print(actions[0].shape)
         actions = np.stack(
             [
                 (
