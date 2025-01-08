b4c497faf54db6cafc744ba54c7854ef03e18d01
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-08 14:47:13 -0800

    Update

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ source ~/.local/bin/env
 source .venv/bin/activate
 huggingface-cli login
 uv run wandb login $API_KEY
-python scripts/train.py --config configs/nav_config.py
+python scripts/train.py --config configs/nav_config.py --tpu
