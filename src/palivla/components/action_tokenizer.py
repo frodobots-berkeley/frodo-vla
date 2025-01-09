bce4117653d2115ce36dd7cdfdc914425a16b0cb
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-09 11:29:49 -0800

    Update

--- src/palivla/components/action_tokenizer.py
+++ src/palivla/components/action_tokenizer.py
@@ -47,8 +47,6 @@ class BinActionTokenizer(ActionTokenizer):
         return self.action_vocab_size
 
     def tokenize(self, data, obs=None):
-        print(data)
-        breakpoint()
         data = (data - self.min_action_value) / (
             self.max_action_value - self.min_action_value
         )
