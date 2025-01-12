64c9ea853564907c8badf518947dfed4122f0b39
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-12 11:50:48 -0800

    Update

--- configs/nav_config.py
+++ configs/nav_config.py
@@ -59,7 +59,7 @@ def get_config():
                 "kwargs": {
                     "optimizer": "sgd",
                     "num_train_steps": num_train_steps,
-                    "base_learning_rate": 1e-6,
+                    "base_learning_rate": 1e-4,
                 },
             },
             "dataset_kwargs": {
