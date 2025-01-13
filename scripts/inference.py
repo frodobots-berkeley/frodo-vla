b47f05fbf64ed310f50741ac8549e2cd40f035cf
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-13 15:17:15 -0800

    adding config

--- scripts/inference.py
+++ scripts/inference.py
@@ -22,9 +22,6 @@ from scalax.sharding import (
     FSDPShardingRule,
     PartitionSpec,
 )
-
-config = get_config()
-
 jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
