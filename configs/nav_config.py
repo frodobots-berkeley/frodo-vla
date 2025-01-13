8e92cb79be1dfd56defa08852db3a8137cb8ffd9
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-13 01:56:45 -0800

    Update

--- configs/nav_config.py
+++ configs/nav_config.py
@@ -19,8 +19,8 @@ def get_config():
             "wandb_mode": "online",
             #Tokenizers
             "language_tokenizer": "google/paligemma-3b-mix-224",
-            "action_tokenizer": "action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=512)",
-            "sequence_builder": "sequence_builder.default(prompt_pad_length=50, gen_pad_length=10)",
+            "action_tokenizer": "action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=256)",
+            "sequence_builder": "sequence_builder.default(prompt_pad_length=75, gen_pad_length=15)",
             # Initialization
             "load_fns": [
                 (
@@ -59,7 +59,7 @@ def get_config():
                 "kwargs": {
                     "optimizer": "adamw",
                     "num_train_steps": num_train_steps,
-                    "base_learning_rate": 1e-7,
+                    "base_learning_rate": 1e-4,
                 },
             },
             "dataset_kwargs": {
@@ -79,11 +79,11 @@ def get_config():
                 "sample_weights": [1.0],
                 "traj_transform_kwargs": {
                     "window_size": 1,
-                    "action_horizon": 1,
+                    "action_horizon": 5,
                 },
                 "frame_transform_kwargs": {
                     "image_augment_kwargs": {},
-                    "resize_size": {"primary": [96, 96]},
+                    "resize_size": {"primary": [224, 224]},
                 },
                 "balance_weights": True,
                 "shuffle_buffer_size": 50000,
