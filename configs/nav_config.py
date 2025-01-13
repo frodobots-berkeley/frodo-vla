7df85c78c21111724d4feccb1ffa9ebe7fc134a8
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-12 17:31:49 -0800

    Update

--- configs/nav_config.py
+++ configs/nav_config.py
@@ -19,7 +19,7 @@ def get_config():
             "wandb_mode": "online",
             #Tokenizers
             "language_tokenizer": "google/paligemma-3b-mix-224",
-            "action_tokenizer": "action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=16)",
+            "action_tokenizer": "action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=256)",
             "sequence_builder": "sequence_builder.default(prompt_pad_length=75, gen_pad_length=15)",
             # Initialization
             "load_fns": [
