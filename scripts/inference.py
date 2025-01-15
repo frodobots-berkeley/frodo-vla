5c9179fcc83c78bde2b49732a53fde7343184d7e
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-14 16:51:08 -0800

    inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -29,7 +29,7 @@ jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 tf.config.set_visible_devices([], "GPU")
 
 def make_sharding(config: ConfigDict):
-    mesh = MeshShardingHelper([-1], ["fsdp"])
+    mesh = MeshShardingHelper([-1], [1])
     sharding_metadata = ShardingMetadata(
         mesh=mesh,
         model_sharding_rule=FSDPShardingRule(),
