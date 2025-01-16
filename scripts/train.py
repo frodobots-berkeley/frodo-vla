b9a019bca6de5cbd9d2d01d802c92e7c0637e8ae
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-15 21:54:27 -0800

    Update

--- scripts/train.py
+++ scripts/train.py
@@ -195,6 +195,7 @@ def main(_):
         for i in pbar:
             if not config.overfit_dataset:
                 batch = next(train_it)
+            print(batch)
             info = model.train_step(batch)
 
             info = jax.device_get(info)
