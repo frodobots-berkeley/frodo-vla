6ef5d9b9e2de6a4088c68df6c77376bde1362ce6
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-17 21:42:21 -0800

    Update

--- src/palivla/standardization_transforms.py
+++ src/palivla/standardization_transforms.py
@@ -866,6 +866,8 @@ METRIC_WAYPOINT_SPACING = {
 }
 def gnm_dataset_transform(trajectory: Dict[str, Any], action_horizon=8) -> Dict[str, Any]:
     traj_len = tf.shape(trajectory["action"])[0]
+
+    print("Action horizon: ", action_horizon)
     # Pad trajectory states
     padding = tf.tile(trajectory["observation"]["state"][-1:, :], [action_horizon, 1])
     trajectory["observation"]["state"] = tf.concat(
