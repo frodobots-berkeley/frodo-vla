6bc0ad455f459bc3463283dfa0cd6783b5f7d697
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-17 19:30:31 -0800

    Update

--- src/palivla/standardization_transforms.py
+++ src/palivla/standardization_transforms.py
@@ -895,7 +895,7 @@ def gnm_dataset_transform(trajectory: Dict[str, Any]) -> Dict[str, Any]:
         ),
         2,
     )
-
+    actions = tf.squeeze(actions, 2)
     normalization_factor = 1.0
     for dataset_name, value in METRIC_WAYPOINT_SPACING.items():
         if tf.strings.regex_full_match(
