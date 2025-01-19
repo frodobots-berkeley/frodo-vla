e0872ed2b70b6730976bbd66de27e26d0ff2ed46
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-19 01:35:08 -0800

    Update

--- configs/nav_config_w_cf.py
+++ configs/nav_config_w_cf.py
@@ -11,7 +11,7 @@ def get_config():
     num_train_steps = FieldReference(100000, int)
 
     model_config = get_default_config()
-    action_horizon = 5
+    action_horizon = 8
     transform = ModuleSpec.create(gnm_dataset_transform, action_horizon=action_horizon)
     return ConfigDict(
         {
@@ -19,7 +19,7 @@ def get_config():
             "wandb_mode": "online",
             #Tokenizers
             "language_tokenizer": "google/paligemma-3b-mix-224",
-            "action_tokenizer": f"action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=256, action_horizon={action_horizon})",
+            "action_tokenizer": f"action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=128, action_horizon={action_horizon})",
             "sequence_builder": "sequence_builder.default(prompt_pad_length=100, gen_pad_length=20)",
             # Initialization
             "load_fns": [
@@ -76,19 +76,19 @@ def get_config():
                         "standardize_fn" : transform,   
                         "force_recompute_dataset_statistics": False,
                     },
-                    "cf_kwargs": {
-                        "name": "cf_dataset",
-                        "data_dir": "gs://vlm-guidance-data",
-                        "image_obs_keys": {"primary": "image"},
-                        "proprio_obs_key": "position",
-                        "language_key" : "language_instruction",
-                        "force_recompute_dataset_statistics": False,
-                        "action_proprio_normalization_type": NormalizationType.NORMAL,
-                        "standardize_fn" : transform,   
-                        "force_recompute_dataset_statistics": False,
-                    },
+                    # "cf_kwargs": {
+                    #     "name": "cf_dataset",
+                    #     "data_dir": "gs://vlm-guidance-data",
+                    #     "image_obs_keys": {"primary": "image"},
+                    #     "proprio_obs_key": "position",
+                    #     "language_key" : "language_instruction",
+                    #     "force_recompute_dataset_statistics": False,
+                    #     "action_proprio_normalization_type": NormalizationType.NORMAL,
+                    #     "standardize_fn" : transform,   
+                    #     "force_recompute_dataset_statistics": False,
+                    # },
                 },
-                "sample_weights": [0.8, 0.2],
+                "sample_weights": [1.0],
                 "traj_transform_kwargs": {
                     "window_size": 1,
                     "action_horizon": action_horizon,
