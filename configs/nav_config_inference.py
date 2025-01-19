ced8aeae3fbb8577aa1dae361f6d50c8fa4ff817
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-19 12:02:42 -0800

    fixinf inf code?

--- configs/nav_config_inference.py
+++ configs/nav_config_inference.py
@@ -32,7 +32,7 @@ def get_config():
                 )
             ],
             "resume_checkpoint_dir": "gs://vlm-guidance-logs/misunderstood-fire-170",
-            "resume_checkpoint_step": 16000,
+            "resume_checkpoint_step": 17000,
             # Overfit
             "overfit_dataset": False,
             # Training settings
