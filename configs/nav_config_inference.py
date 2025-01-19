2425cb4a6c6b30f8f518540dcfb2a7f19691d9f6
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-19 11:48:48 -0800

    fixinf inf code?

--- configs/nav_config_inference.py
+++ configs/nav_config_inference.py
@@ -1,6 +1,6 @@
 from octo.data.utils.data_utils import NormalizationType
 from ml_collections.config_dict import placeholder, ConfigDict, FieldReference
-
+from functools import partial
 from palivla.components.model import get_default_config
 from palivla.standardization_transforms import gnm_dataset_transform
 from octo.utils.spec import ModuleSpec
@@ -11,16 +11,16 @@ def get_config():
     num_train_steps = FieldReference(100000, int)
 
     model_config = get_default_config()
-
-    transform = ModuleSpec.create(gnm_dataset_transform)
+    action_horizon = 8
+    transform = ModuleSpec.create(gnm_dataset_transform, action_horizon=action_horizon)
     return ConfigDict(
         {
             "wandb_project": "vla-nav",
             "wandb_mode": "online",
             #Tokenizers
             "language_tokenizer": "google/paligemma-3b-mix-224",
-            "action_tokenizer": "action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=256)",
-            "sequence_builder": "sequence_builder.default(prompt_pad_length=100, gen_pad_length=30)",
+            "action_tokenizer": f"action_tokenizer.bin(min_action_value=-1, max_action_value=1, action_vocab_size=128, action_horizon={action_horizon})",
+            "sequence_builder": "sequence_builder.default(prompt_pad_length=100, gen_pad_length=20)",
             # Initialization
             "load_fns": [
                 (
@@ -41,7 +41,7 @@ def get_config():
             "num_steps": num_train_steps,
             # Checkpoint settings
             "save_path": "gs://vlm-guidance-logs",
-            "save_interval": 10000,
+            "save_interval": 1000,
             "max_to_keep": 1,
             # Multi-device settings
             "data_axis_size": 1,
@@ -66,7 +66,7 @@ def get_config():
                 "oxe_kwargs": None,
                 "dataset_kwargs_list": {
                     "lcbc_kwargs": {
-                        "name": "lcbc_dataset",
+                        "name": "lcbc_dataset_v2",
                         "data_dir": "gs://vlm-guidance-data",
                         "image_obs_keys": {"primary": "image"},
                         "proprio_obs_key": "position",
@@ -74,12 +74,24 @@ def get_config():
                         "force_recompute_dataset_statistics": False,
                         "action_proprio_normalization_type": NormalizationType.NORMAL,
                         "standardize_fn" : transform,   
+                        "force_recompute_dataset_statistics": False,
                     },
+                    # "cf_kwargs": {
+                    #     "name": "cf_dataset",
+                    #     "data_dir": "gs://vlm-guidance-data",
+                    #     "image_obs_keys": {"primary": "image"},
+                    #     "proprio_obs_key": "position",
+                    #     "language_key" : "language_instruction",
+                    #     "force_recompute_dataset_statistics": False,
+                    #     "action_proprio_normalization_type": NormalizationType.NORMAL,
+                    #     "standardize_fn" : transform,   
+                    #     "force_recompute_dataset_statistics": False,
+                    # },
                 },
                 "sample_weights": [1.0],
                 "traj_transform_kwargs": {
-                    "window_size": 5,
-                    "action_horizon": 5,
+                    "window_size": 1,
+                    "action_horizon": action_horizon,
                 },
                 "frame_transform_kwargs": {
                     "image_augment_kwargs": {},
