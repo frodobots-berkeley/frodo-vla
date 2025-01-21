90576a90ceca983e5bd8d2d25ba8731d5681db9d
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-20 18:15:33 -0800

    adding inf code

--- src/palivla/inference.py
+++ src/palivla/inference.py
@@ -222,7 +222,7 @@ def run_inference(model, prompt, image, config):
     # Predict the output 
     predicted_actions, actions_mask, tokens = model.predict(batch, action_dim=2, action_horizon=action_horizon, return_tokens=True, include_action_tokens=False)
     predicted_actions = predicted_actions[0].squeeze()
-    summed_actions = np.cumsum(predicted_actions*METRIC_WAYPOINT_SPACING["sacson"], axis=1)
+    summed_actions = np.cumsum(predicted_actions, axis=0)
     summed_actions -= summed_actions[0]
 
     # Plot on the image 
