0fa9ea99d9feb382f523ee8a9ae36af78644b405
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-20 21:59:54 -0800

    adding inf code

--- configs/nav_config_inference.py
+++ configs/nav_config_inference.py
@@ -31,7 +31,7 @@ def get_config():
                     },
                 )
             ],
-            "resume_checkpoint_dir": "gs://vlm-guidance-logs/misunderstood-fire-170",
+            "resume_checkpoint_dir": "gs://vlm-guidance-logs/solar-lake-168",
             "resume_checkpoint_step": 66000,
             # Overfit
             "overfit_dataset": False,
