6972cad0750145b60ef3ad8c93c25dd21d322967
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-22 13:30:25 -0800

    testing another model

--- configs/nav_config_inference.py
+++ configs/nav_config_inference.py
@@ -31,8 +31,8 @@ def get_config():
                     },
                 )
             ],
-            "resume_checkpoint_dir": "gs://vlm-guidance-logs/autumn-monkey-178",
-            "resume_checkpoint_step": 40000,
+            "resume_checkpoint_dir": "gs://vlm-guidance-logs/vital-sponge-173",
+            "resume_checkpoint_step": 70000,
             # Overfit
             "overfit_dataset": False,
             # Training settings
