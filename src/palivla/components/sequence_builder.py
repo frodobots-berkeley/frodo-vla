4a35c3b0f6de2b8d58443d35e7c8afb9752c49b7
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-26 13:33:41 -0800

    Update

--- src/palivla/components/sequence_builder.py
+++ src/palivla/components/sequence_builder.py
@@ -27,7 +27,10 @@ class SequenceBuilder:
             return cloudpickle.load(f)
 
     def prepare_prompt(self, language_instruction):
-        language_instructions = np.array([l.decode("utf-8") for l in language_instruction])
+        language_instructions = []
+        for l in language_instruction:
+            if isinstance(l, bytes):
+                language_instructions.append(l.decode("utf-8"))
         language_instructions = language_instructions[language_instructions != ""]
         if language_instructions.shape[0] != 0:
             lang = np.random.choice(language_instructions)
