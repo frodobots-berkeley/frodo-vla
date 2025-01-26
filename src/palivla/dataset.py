d08134c30272ff8bb02d1d1a7449aa59e295ef1b
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-26 01:07:24 -0800

    Update

--- src/palivla/dataset.py
+++ src/palivla/dataset.py
@@ -45,7 +45,7 @@ def make_base_dataset(
 
     dataset = dataset.filter(lambda x: tf.reduce_any(x["observation"]["image_primary"] != 255))
 
-    dataset = dataset.filter(lambda x: x["language_annotation"] != b"")
+    dataset = dataset.filter(lambda x: x["language_instruction"] != b"")
 
     return dataset
 
