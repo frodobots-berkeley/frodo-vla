eeb98ef95e76cd3504fb4140cdac1e1cdbb7348a
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-29 21:09:33 -0800

    Update

--- src/palivla/standardization_transforms.py
+++ src/palivla/standardization_transforms.py
@@ -926,10 +926,10 @@ def gnm_dataset_transform(trajectory: Dict[str, Any], action_horizon=1) -> Dict[
     first_non_zero_values = tf.gather_nd(smooth_pos, gather_indices)
 
     curr_yaw = tf.math.atan2(first_non_zero_values[:, 1] - first_values[:,1], first_non_zero_values[:, 0] - first_values[:,0])
-     Get yaw for each trajectory
-    delta = trajectory["observation"]["state"][1:, :2] - trajectory["observation"]["state"][:-1, :2]
-    yaw = tf.math.atan2(delta[:, 1], delta[:, 0])
-    curr_yaw = tf.pad(yaw, [[0, 1]], constant_values=yaw[-1])
+    # Get yaw for each trajectory
+    # delta = trajectory["observation"]["state"][1:, :2] - trajectory["observation"]["state"][:-1, :2]
+    # yaw = tf.math.atan2(delta[:, 1], delta[:, 0])
+    # curr_yaw = tf.pad(yaw, [[0, 1]], constant_values=yaw[-1])
     # curr_yaw = trajectory["observation"]["yaw"]
     curr_yaw_rotmat = tf.convert_to_tensor(
         [
