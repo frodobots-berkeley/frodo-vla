86b1e3108db1b327c8d9b3333d47f2696f03fe95
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-01-31 18:59:38 -0800

    adding obs only policy

--- eval/eval_random_policy.py
+++ eval/eval_random_policy.py
@@ -10,7 +10,7 @@ import cv2
 policys = {
             #  "cf_filtered" : ("pleasant-hill-251", 100000)
         #    "cf_obs": ("restful-resonance-255", 100000),
-           "obs_only" : ("young-fog-250", 100000),
+           "obs_only" : ("young-fog-250", 90000),
         #    "all" : ("glowing-dust-253", 100000),
            }
 NUM_TRIALS = 3
