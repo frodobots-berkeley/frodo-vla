8baa63f5ab3636c4976feb042737399829f9a9a8
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-02-10 15:45:07 -0800

    debugging fixing dataset

--- debug/rewrite_dataset.py
+++ debug/rewrite_dataset.py
@@ -81,43 +81,27 @@ def fix_dataset(traj, traj_info):
 
 def decode_and_resize(
     obs: dict,
-    resize_size: Union[Tuple[int, int], Mapping[str, Tuple[int, int]]],
 ) -> dict:
     """Decodes images and depth images, and then optionally resizes them."""
     # just gets the part after "image_" or "depth_"
-    image_names = {key[6:] for key in obs if key.startswith("image")}
 
-    if isinstance(resize_size, tuple):
-        resize_size = {name: resize_size for name in image_names}
-
-    for name in image_names:
-        if name not in resize_size:
-            logging.warning(
-                f"No resize_size was provided for image_{name}. This will result in 1x1 "
-                "padding images, which may cause errors if you mix padding and non-padding images."
+    image = obs["image"]
+    if image.dtype == tf.string:
+        if tf.strings.length(image) == 0:
+            # this is a padding image
+            image = tf.zeros((*resize_size.get(name, (1, 1)), 3), dtype=tf.uint8)
+        else:
+            image = tf.io.decode_image(
+                image, expand_animations=False, dtype=tf.uint8
             )
-        image = obs[f"image_{name}"]
-        if image.dtype == tf.string:
-            if tf.strings.length(image) == 0:
-                # this is a padding image
-                image = tf.zeros((*resize_size.get(name, (1, 1)), 3), dtype=tf.uint8)
-            else:
-                image = tf.io.decode_image(
-                    image, expand_animations=False, dtype=tf.uint8
-                )
-        elif image.dtype != tf.uint8:
-            raise ValueError(
-                f"Unsupported image dtype: found image_{name} with dtype {image.dtype}"
-            )
-        if name in resize_size:
-            image = dl.transforms.resize_image(image, size=resize_size[name])
-        obs[f"image_{name}"] = image
+    
+    obs[f"image_decoded"] = image
 
     return obs
 
 def apply_obs_transform(fn: Callable[[dict], dict], frame: dict) -> dict:
     breakpoint()
-    frame["observation"] = fn(frame["observation"])
+    frame["observation_decoded"] = fn(frame["observation"])
     return frame
 
 def main(args):
@@ -127,7 +111,6 @@ def main(args):
     name = args.dataset_name
     builder = tfds.builder(name, data_dir=data_dir)
     dataset = dl.DLataset.from_rlds(builder, split="all", shuffle=False)
-    breakpoint()
     resize_size = (128, 128)
     num_parallel_calls = tf.data.AUTOTUNE
 
