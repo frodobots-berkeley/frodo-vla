ffe6ad948806689b8bd3f2f18614b7cf9f4478fd
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-02-10 17:58:58 -0800

    adding fix

--- debug/rewrite_dataset.py
+++ debug/rewrite_dataset.py
@@ -135,12 +135,12 @@ def reorganize_traj(traj):
                 for step in tf.shape(v2, 0):
                     if len(new_traj["steps"]) <= step:
                         new_traj["steps"].append({})
-                    new_traj["steps"][step][k2] = v2[step]
+                    new_traj["steps"][step][k2] = v2[step, ...]
         except:
             for step in tf.shape(v, 0):
                 if len(new_traj["steps"]) <= step:
                     new_traj["steps"].append({})
-                new_traj["steps"][step][k] = v[step]
+                new_traj["steps"][step][k] = v[step, ...]
 
     new_traj["episode_metadata"] = traj["traj_metadata"]["episode_metadata"]
 
