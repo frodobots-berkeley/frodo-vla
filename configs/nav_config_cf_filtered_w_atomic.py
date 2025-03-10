7b827a590db14295e81d28ad1dd5dc93cc07ef23
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-03-10 13:20:06 -0700

    Update

--- configs/nav_config_cf_filtered_w_atomic.py
+++ configs/nav_config_cf_filtered_w_atomic.py
@@ -217,7 +217,7 @@ def get_config():
                         "force_recompute_dataset_statistics": False,
                     },
                 },
-                "sample_weights": [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+                "sample_weights": [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.4, 0.7, 0.7, 0.7, 0.7, 0.9],
                 "traj_transform_kwargs": {
                     "window_size": 1,
                     "action_horizon": action_horizon,
