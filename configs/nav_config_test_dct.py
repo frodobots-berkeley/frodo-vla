a0b6c6d4f40c3300e29bb42c6bf64ee8501eb81a
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-03-25 15:31:16 -0700

    Update

--- configs/nav_config_test_dct.py
+++ configs/nav_config_test_dct.py
@@ -16,7 +16,7 @@ def get_config():
     return ConfigDict(
         {
             "wandb_project": "vla-nav",
-            "wandb_run"
+            "wandb_run" : "dct_action_tokenizer",
             "wandb_mode": "online",
             #Tokenizers
             "language_tokenizer": "google/paligemma-3b-mix-224",
