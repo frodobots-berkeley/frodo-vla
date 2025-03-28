0ebf43ed8bc91330f5b22c26ed88ec656e665ebd
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-03-28 10:13:03 -0700

    Update

--- configs/nav_config_cf_filtered_skip_norm.py
+++ configs/nav_config_cf_filtered_skip_norm.py
@@ -32,8 +32,8 @@ def get_config():
                     },
                 )
             ],
-            "resume_checkpoint_dir": None,
-            "resume_checkpoint_step": None,
+            "resume_checkpoint_dir": "gs://cat-logs/cf_filtered_data_fixed_2025_03_28_04_24_06",
+            "resume_checkpoint_step": 10000,
             "weights_only": False,
             # Overfit
             "overfit_dataset": False,
