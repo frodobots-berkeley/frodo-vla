9223e13e2150e8795a298a649c7ed7444fc4d889
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-03-31 13:35:46 -0700

    Update

--- configs/nav_config_orig_only_skip_norm.py
+++ configs/nav_config_orig_only_skip_norm.py
@@ -32,8 +32,8 @@ def get_config():
                     },
                 )
             ],
-            "resume_checkpoint_dir": "gs://cat-logs/orig_only_skip_norm_2025_03_30_17_37_42",
-            "resume_checkpoint_step": 40000,
+            "resume_checkpoint_dir": "gs://cat-logs/orig_only_skip_norm_2025_03_31_16_06_47",
+            "resume_checkpoint_step": 45000,
             # Overfit
             "overfit_dataset": False,
             # Training settings
