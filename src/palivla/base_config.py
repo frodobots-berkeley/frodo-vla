abcc840d5504d7933cb76748e24e5f96a47f97e8
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-15 16:20:16 -0700

    Update

--- src/palivla/base_config.py
+++ src/palivla/base_config.py
@@ -18,7 +18,7 @@ def get_config(variant_config: str):
         "wandb_mode": "online",
         # Tokenizers
         "language_tokenizer": "google/paligemma2-3b-pt-224",
-        "action_tokenizer": f"action_tokenizer.dct(action_dim=2, time_horizon={action_horizon}, save_path='tmp', fit=True, pretrained_path=None, default_path='gs://cat-logs/action-tokenizer-dct')",
+        "action_tokenizer": f"action_tokenizer.dct(action_dim=2, time_horizon=8, save_path='tmp', fit=True, pretrained_path=None, default_path='gs://cat-logs/action-tokenizer-dct')",
         "sequence_builder": "sequence_builder.default(prompt_pad_length=50, gen_pad_length=10)",
         # Initialization
         "load_fns": [
