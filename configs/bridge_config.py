e1546be34371a1482b9125684ee36b26654b5b76
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-17 10:21:23 -0700

    Update

--- configs/bridge_config.py
+++ configs/bridge_config.py
@@ -11,6 +11,7 @@ def get_config(variant_config: str = "default"):
 
     config["dataset_kwargs"]["oxe_kwargs"]["data_dir"] = config["data_dir"]
     config["visualization_datasets"]["bridge"]["data_dir"] = config["data_dir"]
+    config["dataset_kwargs"]["oxe_kwargs"]["force_recompute_dataset_statistics"] = False,
     # config["dataset_kwargs"]["oxe_kwargs"]["dataset_statistics"] = "gs://cat-datasets/bridge_release/data/tfds/bridge_dataset/1.0.0/bridge_statistics.json"
 
     return ConfigDict(config)
