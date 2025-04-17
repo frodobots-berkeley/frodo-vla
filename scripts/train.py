2bf78da1eaf4f269403ced360e1e337b2dfb88d6
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-17 10:53:31 -0700

    Update

--- scripts/train.py
+++ scripts/train.py
@@ -274,11 +274,11 @@ def main(_):
                 idxs = np.random.choice(np.arange(eval_plots["pred_actions"].shape[0]//jax.process_count()), 5)
                 gt_viz = eval_plots["gt_actions"][idxs, ...]
                 gt_viz = np.cumsum(gt_viz, axis=1)
-                gt_viz = gt_viz - gt_viz[:, 0, :].reshape(-1, 1, 2)
+                gt_viz = gt_viz - gt_viz[:, 0, :].reshape(-1, 1, model.action_tokenizer.action_dim)
 
                 pred_viz = eval_plots["pred_actions"][idxs, ...]
                 pred_viz = np.cumsum(pred_viz, axis=1)
-                pred_viz = pred_viz - pred_viz[:, 0, :].reshape(-1, 1, 2)
+                pred_viz = pred_viz - pred_viz[:, 0, :].reshape(-1, 1, model.action_tokenizer.action_dim)
                 
                 context = batch["observation"]["image_primary"][idxs, ...]
                 prompts = [model.sequence_builder.prepare_prompt(p) for p in batch["task"]["language_instruction"][idxs]]
