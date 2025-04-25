0b66f8da43280002322ebcaa32fa59622375f135
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-24 17:11:17 -0700

    Update

--- src/palivla/dataset.py
+++ src/palivla/dataset.py
@@ -45,7 +45,7 @@ def make_base_dataset(
     )
     breakpoint()
     dataset = dataset.filter(lambda x: tf.reduce_any(x["observation"]["image_primary"] != 255))
-    dataset = dataset.filter(lambda x: tf.reduce_any(x["observation"]["position"] != 0.0))
+    dataset = dataset.filter(lambda x: tf.reduce_any(x["action"] != 0.0))
 
     return dataset
 
