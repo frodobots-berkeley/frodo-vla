a902bb8f391eb90a6592bb66f4523928d702818c
Catherine Glossop <glossopcatherine@gmail.com>
2025-07-26 20:14:14 -0700

    Update

--- tpu_config_cat.sh
+++ tpu_config_cat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Activate ssh key 
-mkdir -p /home/noam/.ssh
-mv id_ed25519 /home/noam/.ssh
+mkdir -p /home/$USER/.ssh
+mv id_ed25519 /home/$USER/.ssh
 eval "$(ssh-agent -s)"
 ssh-add id_ed25519
-touch /home/noam/.ssh/known_hosts
-ssh-keyscan github.com >> /home/noam/.ssh/known_hosts
+touch /home/$USER/.ssh/known_hosts
+ssh-keyscan github.com >> /home/$USER/.ssh/known_hosts
 sudo tar -xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 # Need to set token for ngrok
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -27,14 +27,14 @@ uv venv --python=python3.11.12
 uv sync --extra tpu  
 
 # For inference
-uv pip install opencv-python
-sudo apt-get install libgl1 -y
-uv pip install Flask
-uv pip install flask-ngrok
-uv pip install ngrok
+# uv pip install opencv-python
+# sudo apt-get install libgl1 -y
+# uv pip install Flask
+# uv pip install flask-ngrok
+# uv pip install ngrok
 uv pip install google-cloud-logging
 uv pip install google-cloud-storage
-sudo cp /tmp/ngrok/ngrok /usr/local/bin
+# sudo cp /tmp/ngrok/ngrok /usr/local/bin
 # Modify the flask_ngrok file to use path /usr/bin
 
 # for inference - need to move ngrok from /tmp/tmp/ngrok to /usr
