8680dc3375d9d838484c6458fb16651b6a8c3f53
Catherine Glossop <glossopcatherine@gmail.com>
2025-08-30 20:55:27 -0700

    fixing path

--- src/palivla/frodo_dataset.py
+++ src/palivla/frodo_dataset.py
@@ -272,7 +272,7 @@ class FrodbotDataset_MBRA(LeRobotDataset):
             print("Using local dataset or GCP bucket...")
             # with tf.io.gfile.GFile(Path(root) / "frodobots_dataset" / "dataset_cache.zarr") as f:
             print(root)
-            self.dataset_cache = xr.open_zarr(root / "frodobots_dataset" / "dataset_cache.zarr")
+            self.dataset_cache = xr.open_zarr(f"{root}/frodobots_dataset/dataset_cache.zarr")
             breakpoint()
 
         super().__init__(
