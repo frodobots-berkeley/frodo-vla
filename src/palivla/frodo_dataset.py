d0fcc65c41a9e7d6b14737c7373159230d725173
Catherine Glossop <glossopcatherine@gmail.com>
2025-09-07 21:27:06 -0700

    make dataloading synchronous

--- src/palivla/frodo_dataset.py
+++ src/palivla/frodo_dataset.py
@@ -277,7 +277,7 @@ class FrodbotDataset_MBRA:
             print("Using local dataset or GCP bucket...")
             zarr_path = f"{root}/frodobots_dataset/dataset_cache.zarr"
             fs = fsspec.filesystem("gcs", asynchronous=False)
-            store = sync(fs.get_mapper(zarr_path))
+            store = Sync(fs.get_mapper(zarr_path))
             self.dataset_cache = zarr.open_group(store, mode='r')
 
         # super().__init__(
